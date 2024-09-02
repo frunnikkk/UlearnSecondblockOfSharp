aaaf50de495aca98f247fe151efeaacfc3a1a194
frunnikk <frunnikk@gmail.com>
2024-09-03 00:19:34 +0300

    Made task number 5 with recursion

--- UlearnSecondBlockofSharp/Program.cs
+++ UlearnSecondBlockofSharp/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> years = [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004];
+        int i = 0;
+        int numberOfYears = 0;
+        int result = GetLeapYear(years, i, numberOfYears);
+        Console.Write($"{result}");
+    }
 
-Console.WriteLine("Hello, World!");
+    public static int GetLeapYear(List<int> a, int i, int b)
+    {
+        int result = b;
+        if (i > a.Count - 1)
+        {
+            return result;
+        }
+        else
+        {
+            if (a[i] % 4 == 0)
+            {
+                return GetLeapYear(a, i + 1, result + 1);
+            }
+            else
+            {
+                return GetLeapYear(a, i + 1, result);
+            }
+        }
+    }
+}
